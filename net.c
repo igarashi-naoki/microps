49492a59c4a3be0d4533640fbb890c29afb2fdf8
igarashi-naoki <naoki17igarashi@gmail.com>
2026-01-15 19:16:45 +0000

    completion of step12

--- net.c
+++ net.c
@@ -7,6 +7,7 @@
 #include "util.h"
 #include "net.h"
 
+#include "arp.h"
 #include "ip.h"
 #include "icmp.h"
 
@@ -201,6 +202,10 @@ net_init(void)
     infof( "initialize...");
     if (platform_init() == -1) {
         errorf( "platform_init() failure");
+        return -1;
+    }
+    if (arp_init() == -1) {
+        errorf( "arp_init() failure");
         return -1;
     }
     if(ip_init() == -1) {
